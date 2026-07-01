d271176b5e2e7566aaa80dce25df7f49dc3881bf
Patrick <petrik09@live.nl>
2026-07-01 15:40:12 +0200

    refactor: formatting

--- lua/butter/autocmds.lua
+++ lua/butter/autocmds.lua
@@ -16,11 +16,7 @@ function M.setup()
   local group = vim.api.nvim_create_augroup("Butter", { clear = true })
 
   if config.opts.auto_open then
-    vim.api.nvim_create_autocmd("VimEnter", {
-      callback = open,
-      desc = "Open Butter when Neovim is opened with a directory",
-      group = group,
-    })
+    vim.api.nvim_create_autocmd("VimEnter", { callback = open, desc = "Open Butter when Neovim is opened with a directory", group = group })
   end
 end
 
